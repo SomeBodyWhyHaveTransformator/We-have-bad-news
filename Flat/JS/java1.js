d7246b59c5f7d077456a2ae0afaa9d306df6d46c
ivan.2013.ie@mail.ru <ivan.2013.ie@mail.ru>
2017-02-02 19:05:11 +0200

    Grand Master Oogway

--- Flat/JS/java1.js
+++ Flat/JS/java1.js
@@ -24,7 +24,7 @@ function PE2_out(){
 function PE2_click(){
 	document.getElementById("PE2").style = ("background: white;");
 	document.getElementById("Mmenu").style = ("background: white;")
-	document.getElementById("Mmenu").innerHTML = ("<div class='PE'><div id='PE1' onmouseover='PE1_over()' onmouseout='PE1_out()' onclick='PE1_click()'>Чип и Дейл видео</div><div id='PE2' onmouseover='PE2_over()' onmouseout='PE2_out()' onclick='PE2_click()'>Связатся с нами</div><div id='PE3' onmouseover='PE3_over()' onmouseout='PE3_out()' onclick='PE3_click()'>Фото Шерлока Холмса</div><div id='PE4'></div></div><img src='mail.png'><p>Mail: ivan.2013.ie@mail.ru</p><img src='vk.png'><p>ВКонтакте: <a href='https://vk.com/ivanpisckun'>Іван Піскун</a></p><img src='pg.png'><p>PlayGround: <a href='http://users.playground.ru/2136883/'>Lord of nord</a></p>")
+	document.getElementById("Mmenu").innerHTML = ("<div class='PE'><div id='PE1' onmouseover='PE1_over()' onmouseout='PE1_out()' onclick='PE1_click()'>Чип и Дейл видео</div><div id='PE2' onmouseover='PE2_over()' onmouseout='PE2_out()' onclick='PE2_click()'>Связатся с нами</div><div id='PE3' onmouseover='PE3_over()' onmouseout='PE3_out()' onclick='PE3_click()'>Фото Шерлока Холмса</div><div id='PE4'></div></div><img src='image/mail.png'><p>Mail: ivan.2013.ie@mail.ru</p><img src='image/vk.png'><p>ВКонтакте: <a href='https://vk.com/ivanpisckun'>Іван Піскун</a></p><img src='image/pg.png'><p>PlayGround: <a href='http://users.playground.ru/2136883/'>Lord of nord</a></p>")
 }
 function PE3_over(){
 	document.getElementById("PE3").style = ("background: white;");
@@ -35,5 +35,5 @@ function PE3_out(){
 function PE3_click(){
 	document.getElementById("PE3").style = ("background: white;");
 	document.getElementById("Mmenu").style = ("background: white;")
-	document.getElementById("Mmenu").innerHTML = ("<div class='PE'><div id='PE1' onmouseover='PE1_over()' onmouseout='PE1_out()' onclick='PE1_click()'>Чип и Дейл видео</div><div id='PE2' onmouseover='PE2_over()' onmouseout='PE2_out()' onclick='PE2_click()'>Связатся с нами</div><div id='PE3'  onmouseover='PE3_over()' onmouseout='PE3_out()' onclick='PE3_click()'>Фото Шерлока Холмса</div><div id='PE4'></div></div><img src='sher.jpg'>")
+	document.getElementById("Mmenu").innerHTML = ("<div class='PE'><div id='PE1' onmouseover='PE1_over()' onmouseout='PE1_out()' onclick='PE1_click()'>Чип и Дейл видео</div><div id='PE2' onmouseover='PE2_over()' onmouseout='PE2_out()' onclick='PE2_click()'>Связатся с нами</div><div id='PE3'  onmouseover='PE3_over()' onmouseout='PE3_out()' onclick='PE3_click()'>Фото Шерлока Холмса</div><div id='PE4'></div></div><img src='image/sher.jpg'>")
 }
