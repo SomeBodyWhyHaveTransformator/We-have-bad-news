dff449ad18be9e3201e0f70d0b8cda746e0b88be
ivan.2013.ie@mail.ru <ivan.2013.ie@mail.ru>
2017-02-09 21:10:23 +0200

    Upload

---
+++ Portfolio/JavaScript/java1.js
@@ -0,0 +1,37 @@
+/* Head_1 */
+function head_on(){
+    document.getElementById("on_the_head").style = ("background: white;")
+}
+function head_out(){
+	document.getElementById("on_the_head").style = ("background: none;")
+}
+/* head_2 */
+function head_on1(){
+    document.getElementById("on_the_head1").style = ("background: white;")
+}
+function head_out1(){
+	document.getElementById("on_the_head1").style = ("background: none;")
+}
+/* head_3 */
+function head_on2(){
+    document.getElementById("on_the_head2").style = ("background: white;")
+}
+function head_out2(){
+	document.getElementById("on_the_head2").style = ("background: none;")
+}
+/* head_4 */
+function head_on3(){
+    document.getElementById("on_the_head3").style = ("background: white;")
+}
+function head_out3(){
+	document.getElementById("on_the_head3").style = ("background: none;")
+}
+function WeCan(){
+	window.scrollTo(0,700);
+}
+function About(){
+	window.scrollTo(0,300);
+}
+function Contact(){
+	window.scrollTo(0,1700);
+}
